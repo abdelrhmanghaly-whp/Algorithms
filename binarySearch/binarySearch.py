57e2446d4cffc54da1d5cfde24d6a17a7b2ed1fb
Abdelrhman Ghaly <84441904+abdelrhmanghaly-whp@users.noreply.github.com>
2023-01-13 04:10:03 +0200

    Update binarySearch.py

--- binarySearch/binarySearch.py
+++ binarySearch/binarySearch.py
@@ -1,1 +1,20 @@
+def binarySearch(list, requiredNumber):
+    left = 0 
+    right = len(list)-1
+    
+    while left < right:
+        mid = int((left+right)/2)
+        check=list[mid]
+
+        if check == requiredNumber:
+            return mid
+        elif check > requiredNumber:
+            right = mid -1
+        else:
+            left = mid +1
+
+    return "Wasn't found"
+
+ex = []
+print(binarySearch())
 
